88dd38859be1dda4367550829394da6aa3320088
wengqiuhuang <94047514+wengqiuhuang@users.noreply.github.com>
2021-11-10 21:59:44 +0800

    pulltest

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git5");
         System.out.println("hello git6");
         System.out.println("hello git6  push test");
+        System.out.println("hello git6  push test");
     }
 }
