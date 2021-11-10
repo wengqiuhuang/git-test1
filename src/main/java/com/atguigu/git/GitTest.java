45ee38c906926ddd7612ac99363d9020f99e1dd7
wqh <1648988492@qq.com>
2021-11-10 21:37:57 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("hello git3");
         System.out.println("hello git5");
         System.out.println("hello git6");
+        System.out.println("hello git6  push test");
     }
 }
